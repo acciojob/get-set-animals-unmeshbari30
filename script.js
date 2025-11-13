161e4f72ee5c61232bd7ae98bbc2d680d24f1c93
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-13 22:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species
+	}
 
-class Dog extends Animal {}
+	makeSound(){
+			return `The {this.species} makes a sound`
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log('woof')
+	}
+	
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log('purr')
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
